4ec4fa6a11132774d8236585b2897e4f3fcd89a8
Joel Burget <joelburget@gmail.com>
2018-06-29 11:19:51 -0700

    tmp

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
       options: {
         plugins: [
           'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
         ],
       },
